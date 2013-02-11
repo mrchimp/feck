b4318f549ea6000e117009914ef974a3d5138e1f
mrchimp <chimpytk@gmail.com>
2013-02-11 00:46:16 +0000

    put everything in a wrapper

--- feck.py
+++ feck.py
@@ -8,68 +8,57 @@ import os
 prompt_str = "Say > "
 chatmode_str = "Chat Mode - type /quit or /q to leave"
 
-screen = curses.initscr()
+class Feck:
 
-screen_y, screen_x = screen.getmaxyx()
+    def __init__(self):
+        curses.wrapper(self.main)
+        print('Bye then.')
 
-curses.noecho()
-curses.cbreak()
-screen.keypad(1)
+    def main(self, screen):
+        curses.use_default_colors()
+        self.screen_y, self.screen_x = screen.getmaxyx()
+        
+        self.status_win = curses.newwin(1, self.screen_x, 0, 0)
+        self.status_win.addstr(chatmode_str)
+        self.status_win.refresh()
 
+        self.input_win = curses.newwin(1, self.screen_x, 1, 0)
+        self.input_win.addstr(0,0,prompt_str)
+        self.input_win.refresh()
 
+        self.output_win = curses.newpad(20, self.screen_x)
+        self.output_win.refresh(0,0, 2,0, 20,20)
+        
+        while True:
+            curses.echo()
+            input = self.input_win.getstr(0, 6, 60)
+            curses.noecho()
+            input = input.decode(sys.stdout.encoding)
+            if input == '/quit' or input == '/q':
+                self.showStatus('Are you sure you want to quit? y/n ')
+                event = self.input_win.getch()
+                if event == ord('y'): 
+                    break
+                else:
+                    self.showStatus(chatmode_str)
+                    self.input_win.clear()
+                    self.input_win.addstr(0,0, prompt_str)
+                    self.input_win.refresh()
+                    continue
+            self.input_win.clear()
+            self.input_win.addstr(0,0, prompt_str)
+            self.input_win.refresh()
+            self.showMessage(input)
+        
+    def showStatus(self, msg):
+        self.status_win.clear()
+        self.status_win.refresh()
+        self.status_win.addstr(msg + (" "*(self.screen_x - len(msg) - 1)), curses.A_BLINK)
+        self.status_win.refresh()    
 
-status_win = curses.newwin(1, screen_x, 0, 0)
-status_win.addstr(chatmode_str)
-status_win.refresh()
-
-input_win = curses.newwin(1, screen_x, 1, 0)
-input_win.addstr(0,0,prompt_str)
-input_win.refresh()
-
-output_win = curses.newpad(20, screen_x)
-output_win.refresh(0,0, 2,0, 20,20)
-
-def showStatus(msg):
-    status_y, status_x = status_win.getmaxyx()
-    status_win.clear()
-    status_win.refresh()
-    status_win.addstr(msg + (" "*(screen_x - len(msg) - 1)), curses.A_REVERSE)
-    #status_win.addstr(msg + str(screen_x) + "-" + str(len(msg)) + "-" + str(screen_x - len(msg)), curses.A_REVERSE)
-    status_win.refresh()    
-
-def showMessage(message):
-    output_win.addstr(message + "\n")
-    output_win.refresh(0,0, 2,0, 20,20)
-
-while True: 
-    # event = screen.getch()
-    # if event == ord('q'): break
-    curses.echo()
-    input = input_win.getstr(0, 6, 60)
-    curses.noecho()
-    input = input.decode(sys.stdout.encoding)
-    if input == '/quit' or input == '/q':
-        #input_win.clear()
-        showStatus('Are you sure you want to quit? y/n ')
-        #screen.refresh()
-        event = input_win.getch()
-        if event == ord('y'): 
-            break
-        else:
-            showStatus(chatmode_str)
-            input_win.clear()
-            input_win.addstr(0,0, prompt_str)
-            input_win.refresh()
-            continue
-    input_win.clear()
-    input_win.addstr(0,0, prompt_str)
-    input_win.refresh()
-    showMessage(input)
-    
-screen.clear()
-curses.nocbreak()
-screen.keypad(0)
-curses.echo()
-curses.endwin()
-os.system('cls')
-print('Bye then.')
+    def showMessage(self, message):
+        self.output_win.addstr(message + "\n")
+        self.output_win.refresh(0,0, 2,0, 20,20)
+        
+if __name__ == '__main__':
+    feck = Feck()
